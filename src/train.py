256f459ca327f66c7532ac9b6d7896f102c29238
nMaax <massimilianocarli@proton.me>
2025-05-12 09:21:43 +0200

    improved training functions with new merging approaches for input data, and improved verbosity

--- src/train.py
+++ src/train.py
@@ -1,42 +1,76 @@
 import os
 import time
 import warnings
+from tqdm import tqdm
+import numpy as np
 import torch
 from sklearn.metrics import roc_auc_score
 from collections import defaultdict
 from src.utils import minmax_normalize, z_score_normalize, flatten_and_concat, adjust_time_series_size
 from src.save import save_model_checkpoint
 
-def train_one_epoch(model, dataloader, optimizer, criterion, verbose=False):
+def train_one_epoch(model, dataloader, optimizer, criterion, merge_strategy='default', break_at_batch=None, verbose=False):
     """Train the model for one epoch"""
     device = next(model.parameters()).device  # Get the device of the model
     
     model.train()  # Set the model to training mode
     running_loss = 0.0  # Initialize running loss
 
-    for batch_idx, (mic, acc, gyro, labels) in enumerate(dataloader):
-        # Normalize input tensors
-        mic_norm = z_score_normalize(mic)
-        acc_norm = z_score_normalize(acc)
-        gyro_norm = z_score_normalize(gyro)
+    len_dataloader = len(dataloader)
+    if not break_at_batch:
+        break_at_batch = len_dataloader
+    iter = dataloader
+    if verbose == 1:
+        iter = tqdm(dataloader, desc="Training", unit="batch") if verbose else dataloader
 
-        # Flatten and concatenate inputs
-        inputs = flatten_and_concat(mic_norm, acc_norm, gyro_norm).to(device)
+    for batch_idx, (mic, acc, gyro, labels) in enumerate(iter):
+        if merge_strategy in {'default', 'flatten'}:
+            # Normalize input tensors
+            mic_norm = z_score_normalize(mic)
+            acc_norm = z_score_normalize(acc)
+            gyro_norm = z_score_normalize(gyro)
+
+            # Flatten and concatenate inputs
+            inputs = flatten_and_concat(mic_norm, acc_norm, gyro_norm).to(device)
+        elif merge_strategy == 'stack':
+            # Normalize input tensors
+            mic_norm = z_score_normalize(mic)
+            acc_norm = z_score_normalize(acc)
+            gyro_norm = z_score_normalize(gyro)
+
+            # Adjust time series size for acc and gyro to match mic
+            acc_adjusted = adjust_time_series_size(acc_norm, mic_norm.shape[1], 'balanced')
+            gyro_adjusted = adjust_time_series_size(gyro_norm, mic_norm.shape[1], 'balanced')
+
+            # Stack inputs along the feature dimension
+            inputs = torch.cat([mic_norm, acc_adjusted, gyro_adjusted], dim=2).to(device)
+        else:
+            raise ValueError(f"Unknown merge_strategy: {merge_strategy}")
+
+        if verbose >= 3:
+            print(f"Input to the model of shape: {inputs.shape}")
+        
+        outputs = model(inputs)  # Forward pass
+        
+        if verbose >= 3:
+            print(f"Output from the model of shape: {outputs.shape}")
 
         optimizer.zero_grad()  # Reset gradients
-        outputs = model(inputs)  # Forward pass
-        loss = criterion(outputs, inputs)  # Compute loss
+        loss = criterion(inputs, outputs)  # Compute loss
         loss.backward()  # Backward pass
         optimizer.step()  # Update model parameters
 
         running_loss += loss.item()  # Accumulate loss
 
-        if verbose:
-            print(f"Batch {batch_idx + 1}/{len(dataloader)} | Loss: {loss.item():.6f}")
+        if verbose >=2 and batch_idx % (10 if verbose == 2 else 1):
+            print(f"[Batch {batch_idx + 1}/{len_dataloader}] Loss: {loss.item():.6f}")
 
-    return running_loss / len(dataloader)  # Return average loss for the epoch
+        if batch_idx >= break_at_batch:
+            break
 
-def evaluate(model, dataloader, criterion, sensors_to_test=None, verbose=False):
+    return running_loss / break_at_batch  # Return average loss for the epoch
+
+def evaluate(model, dataloader, criterion, sensors_to_test=None, merge_strategy='default', verbose=False):
     """Function to evaluate the model on a validation dataset"""
     device = next(model.parameters()).device  # Get the device of the model
     
@@ -48,22 +82,44 @@ def evaluate(model, dataloader, criterion, sensors_to_test=None, verbose=False):
     segment_errors = defaultdict(list)  # Dictionary to store errors per segment
     segment_labels = {}  # Dictionary to store labels per segment
 
+    # Wrap the dataloader with tqdm if verbose is enabled
+    len_dataloader = len(dataloader)
+    iter = dataloader
+    if verbose == 1:
+        iter = tqdm(dataloader, desc="Evaluation", unit="batch") if verbose else dataloader
+
     with torch.no_grad():  # Disable gradient computation
-        for mic, acc, gyro, labels in dataloader:
+        for batch_idx, (mic, acc, gyro, labels) in enumerate(iter):
             # Mask sensors not requested for testing
             mic = mic if 'mic' in sensors_to_test else torch.zeros_like(mic)
             acc = acc if 'acc' in sensors_to_test else torch.zeros_like(acc)
             gyro = gyro if 'gyro' in sensors_to_test else torch.zeros_like(gyro)
 
-            # Normalize input tensors
-            mic_norm = z_score_normalize(mic)
-            acc_norm = z_score_normalize(acc)
-            gyro_norm = z_score_normalize(gyro)
+            if merge_strategy in {'default', 'flatten'}:
+                # Normalize input tensors
+                mic_norm = z_score_normalize(mic)
+                acc_norm = z_score_normalize(acc)
+                gyro_norm = z_score_normalize(gyro)
 
-            # Flatten and concatenate inputs
-            inputs = flatten_and_concat(mic_norm, acc_norm, gyro_norm).to(device)
+                # Flatten and concatenate inputs
+                inputs = flatten_and_concat(mic_norm, acc_norm, gyro_norm).to(device)
+            elif merge_strategy == 'stack':
+                # Normalize input tensors
+                mic_norm = z_score_normalize(mic)
+                acc_norm = z_score_normalize(acc)
+                gyro_norm = z_score_normalize(gyro)
+
+                # Adjust time series size for acc and gyro to match mic
+                acc_adjusted = adjust_time_series_size(acc_norm, mic_norm.shape[1], 'balanced')
+                gyro_adjusted = adjust_time_series_size(gyro_norm, mic_norm.shape[1], 'balanced')
+
+                # Stack inputs along the feature dimension
+                inputs = torch.cat([mic_norm, acc_adjusted, gyro_adjusted], dim=2).to(device)
+            else:
+                raise ValueError(f"Unknown merge_strategy: {merge_strategy}")
+
             outputs = model(inputs)  # Forward pass
-            loss = criterion(outputs, inputs)  # Compute loss
+            loss = criterion(inputs, outputs)  # Compute loss
             val_loss += loss.item()  # Accumulate validation loss
 
             # Compute reconstruction errors for each sample
@@ -82,10 +138,15 @@ def evaluate(model, dataloader, criterion, sensors_to_test=None, verbose=False):
                 segment_errors[seg_id].append(err)
                 segment_labels[seg_id] = lbl
 
+            if verbose >=2 and batch_idx % (10 if verbose == 2 else 1):
+                median_error = np.median(batch_errors)  # Aggregate errors using median
+                average_error = np.mean(batch_errors)  # Aggregate errors using mean
+                print(f"[Batch {batch_idx+1}/{len_dataloader}] Validation | Loss: {loss.item():.6f} | Median Batch Error: {median_error:.6f} | Average Batch Error: {average_error:.6f}")
+
     val_loss /= len(dataloader)  # Compute average validation loss
 
     # Compute median error per segment
-    med_errors = [torch.median(torch.tensor(errors)).item() for errors in segment_errors.values()]
+    med_errors = [torch.median(torch.tensor(np.array(errors))).item() for errors in segment_errors.values()]
     med_labels = [segment_labels[seg_id] for seg_id in segment_errors.keys()]
     
     # Handle edge case where only one class is present
@@ -100,9 +161,9 @@ def evaluate(model, dataloader, criterion, sensors_to_test=None, verbose=False):
     
     return val_loss, val_auc  # Return validation loss and AUC
 
-def train_model(name, model, criterion, optimizer, train_loader, val_loader,
+def train_model(name, model, criterion, optimizer, train_loader, val_loader, merge_startegy='default',
                 start_epoch=0, num_epochs=10, save_every=1, 
-                save_dir='checkpoints', verbose=True):
+                save_dir='checkpoints', verbose=True, break_at_batch=None):
     """Function to train the model for multiple epochs"""
     train_losses, val_losses, val_aucs = [], [], []  # Initialize lists to store metrics
 
@@ -120,8 +181,11 @@ def train_model(name, model, criterion, optimizer, train_loader, val_loader,
         start_time = time.time()
 
         # Train for one epoch and evaluate on validation set
-        train_loss = train_one_epoch(model, train_loader, optimizer, criterion, verbose)
-        val_loss, val_auc = evaluate(model, val_loader, criterion, ['mic', 'acc', 'gyro'], verbose)
+        train_loss = train_one_epoch(model, train_loader, optimizer, criterion, merge_startegy, break_at_batch, verbose)
+        if val_loader:
+            val_loss, val_auc = evaluate(model, val_loader, criterion, ['mic', 'acc', 'gyro'], merge_startegy, verbose)
+        else:
+            val_loss, val_auc = 0.0, 0.0
 
         # End timing the epoch
         epoch_time = time.time() - start_time
